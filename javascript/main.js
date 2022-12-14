b360e681813e255ddb08bed3c294eac1c865a90e
natanael leites <nathaxx09@hotmail.com>
2022-12-13 22:10:28 -0300

    objetos-vacio

---
+++ javascript/main.js
@@ -0,0 +1,43 @@
+/* Objetos */
+
+let producto1 = {
+    precio: 1590,
+    nombre: "Bolsa de Boxeo",
+    color: "Azul",
+    stock: 6,
+    descripcion: "Bolsa muy resistente. Se puede llenar con arena, retazos de tela, o espuma de varias densidades"
+}
+
+let producto2 = {
+    precio: 2455,
+    nombre: "Bicicleta de spinning",
+    color: "Negro",
+    stock: 3,
+    descripcion: "Bicicleta fija para ejercicios, con regulador de instensidad"
+}
+
+let producto3 = {
+    precio: 330,
+    nombre: "Inflador Multi",
+    color: "Plateado",
+    stock: 16,
+    descripcion: "Mini-inflador para bicicleta adosable, con accesorio para inflar pelotas"
+}
+
+let producto4 = {
+    precio: 450,
+    nombre: "Cinta para estiramiento",
+    color: "Azul",
+    stock: 21,
+    descripcion: "Banda elastica de alta resistencia, durable, absorcion de humedad y sudor"
+}
+
+let producto5 = {
+    precio: 144,
+    nombre: "Hand grip",
+    color: "Negro",
+    stock: 8,
+    descripcion: "Articulo de estimulacion manual en forma de V. Ideal para casos de esguinces de muñeca, tendones cortos o recuperacion de musculos de la mano"
+}
+/* =============================== */
+
